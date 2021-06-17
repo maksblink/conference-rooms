46d58408cbda202c2965bb1a7fdeb3794701c29b
maksblink <blinkmaks777@gmail.com>
2021-06-17 15:03:18 +0200

    templates

--- conference_rooms/conference_rooms/settings.py
+++ conference_rooms/conference_rooms/settings.py
@@ -76,10 +76,8 @@ WSGI_APPLICATION = 'conference_rooms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # <- usunac?
-        'NAME': BASE_DIR / 'db.sqlite3', # <- usunac?
         'HOST': '127.0.0.1',
-        'NAME': 'conferance_rooms',
+        'NAME': 'conference_rooms',
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'USER': 'postgres',
         'PASSWORD': 'coderslab',
--- conference_rooms/main_app/views.py
+++ conference_rooms/main_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
 
-# Create your views here.
+
+class AddRoom(View):
+    def get(self, request):
+        return render(request, 'main_app/addroom.html')
+
+    def post(self, request):
+        pass
